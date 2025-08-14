83828fd4a15285bb3da27cdb8e51d871946a8165
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-15 00:03:58 +0530

    add.cpp

---
+++ basic/add.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the number ";
+    cin>>a;
+    cout<<"enter the second number is ";
+    cin>>b;
+    cout<<"sum of the number is "<<a+b;
+}
