c0eb6be19ad555cd9adcd97458fd05c318ec52d9
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-15 13:14:36 +0530

    inside the character

--- basic/character.cpp
+++ basic/character.cpp
@@ -1,3 +1,11 @@
 #include<iostream>
 using namespace std;
-int main()
+int main(){
+    int a=7;
+    int b=7;
+    char c='u';
+    cout<<"this the tutorial is 4.the value a is ="<<a<<"\nthe value b is="<<b;
+    cout<<"\nthe value c is ="<<c;
+    return 0;
+
+}
