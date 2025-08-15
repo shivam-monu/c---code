db731966b1fb406ad0511d19504d1be608516385
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-15 11:22:53 +0530

    float

---
+++ basic/float.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=6,b=52;
+    float pi=3.15;
+    cout<<"this tutorial 4. here the value of a is"<<a<<".the value of b is "<<b;
+    cout<<"the value of pi is:"<<pi;
+}
