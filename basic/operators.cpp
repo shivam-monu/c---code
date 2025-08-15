d3391933ce42a00ba2544ca19c38e2b3c1bbdfdd
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-15 16:31:16 +0530

    operator

---
+++ basic/operators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4,b=6;
+    cout<<"operator in c++\n";
+    cout<<"following are the type of operators in c++"<<endl;
+    //Arithmatics operators
+    cout<<"the value of a+b is"<<a+b<<endl;
+    cout<<"the value of a-b is"<<a-b<<endl;
+    cout<<"the value of a*b is"<<a*b<<endl;
+    cout<<"the value of a/b is"<<a/b<<endl;
+    cout<<"the value of a%b is"<<a%b<<endl;
+    cout<<"the value of a++ is"<<a++<<endl;
+    cout<<"the value of a-- is"<<a--<<endl;
+    cout<<"the value of ++a is"<<++a<<endl;
+    cout<<"the value of --a is"<<--a<<endl;
+    cout<<"the value of b++ is"<<b++<<endl;
+    cout<<"the value of b-- is"<<b--<<endl;
+    cout<<"the value of ++b is "<<++b<<endl;
+    cout<<"the value of --b is "<<--b<<endl; 
+    return 0;
+
+}
