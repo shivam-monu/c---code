a5f9eabb40372e7fad840700e7910d7521b5e898
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-15 11:12:32 +0530

    value

---
+++ basic/putvalue.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=7;
+    int b=8;
+    cout<<"this is tutorial 4 .here the value of a is "<<a<<".the value of b is"<<b;
+    return 0;
+}
