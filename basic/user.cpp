01e9ab13395bfbbbdf1e2da5dcf5f5351f2a8ef0
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-15 14:25:26 +0530

    user

---
+++ basic/user.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"enter the  first number=\n";
+    cin>>num1;
+    cout<<"enter the second number=\n";
+    cin>>num2;
+    cout<<"the sum ="<<num1+num2;
+    return 0;
+
+}
